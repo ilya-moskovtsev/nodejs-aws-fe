0e6bd98af9fd809e61b4753751cbbb4e12399110
Ilia Moskovtcev <Ilia_Moskovtcev@epam.com>
2020-12-06 21:32:15 +0300

    update cart url

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -4,7 +4,7 @@ const API_PATHS = {
   order: 'https://sy6ni7vd5i.execute-api.us-east-1.amazonaws.com/dev',
   import: 'https://peub31gawe.execute-api.us-east-1.amazonaws.com/dev',
   bff: 'https://sy6ni7vd5i.execute-api.us-east-1.amazonaws.com/dev',
-  cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  cart: 'http://ilya-moskovtsev-cart-api-cart-api-develop.us-east-1.elasticbeanstalk.com/api',
 };
 
 export default API_PATHS;
